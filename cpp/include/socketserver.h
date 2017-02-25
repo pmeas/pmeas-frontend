88576a355099dec649c5385a8614874a5019881e
Druage <Druage@gmx.com>
2017-02-24 21:04:55 -0500

    Added comments to the c++ code.

--- cpp/include/socketserver.h
+++ cpp/include/socketserver.h
@@ -2,12 +2,21 @@
 
 #include <QObject>
 
-#include <QLocalServer>
+// The SocketServer class will allow us to listen to and
+// send messages to the backend server.
+//
+// This class is exposed to the QML environment so
+// it can be used and tied into the GUI easily.
+
+// Forward declares
+class QLocalServer;
 
 class SocketServer : public QObject
 {
+    // This Q_OBJECT macro is required by Qt, because we subclass the QObject class.
     Q_OBJECT
 public:
+    // Define a basic contructor for a QObject
     explicit SocketServer( QObject *parent = nullptr );
 
 signals:
@@ -15,5 +24,8 @@ signals:
 public slots:
 
 private:
+    // This is a raw pointer but does not need to be manually deleted.
+    // It gets parented to this SocketServer class which will detroy it
+    // automatically.
     QLocalServer *m_localServer;
 };
--- main.cpp
+++ main.cpp
@@ -5,13 +5,19 @@
 
 int main(int argc, char *argv[])
 {
+
+    // Sets up the GUI environment, this is required to a GUI application.
     QGuiApplication app(argc, argv);
 
+    // Loads up the the QML javascript engine
     QQmlApplicationEngine engine;
 
     qmlRegisterType<SocketServer>( "LocalServer", 1, 0, "SocketServer" );
 
+    // Loads the starting point file of the GUI.
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
+    // Activates the event loop.
+    // It basically just is a big while loop and keeps the window alive.
     return app.exec();
 }
