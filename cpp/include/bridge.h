e97581f1ad1725fa4c466ae252a6b578f5100f5d
mattfg <matt.gaertner@yahoo.com>
2017-04-15 18:00:27 -0400

    Some comments for req 3.3.3.3; still incomplete

--- cpp/include/bridge.h
+++ cpp/include/bridge.h
@@ -31,6 +31,7 @@ public:
     // Define a basic contructor for a QObject
     explicit Bridge( QObject *parent = nullptr );
 
+    /* Is the bridge's tcp ready to send data? */
     bool connected() const;
 
     // Virtual methods from the QQmlParserStatus class.
@@ -40,18 +41,30 @@ public:
     void componentComplete() override;
 
 signals:
+    /* Signalled when the TCP socket initially connects */
     void tcpSocketConnected();
+    /* Signalled when the TCP socket is unexpectedly lost */
     void lostConnection();
+    /* Signalled when the settings menu receives the list of input devices from the backend */
     void inportsChanged();
+    /* Signalled when the settings menu receives the list of output devices from the backend */
     void outportsChanged();
+    /* Signalled when a new input device is chosen in the settings menu */
     void curInChanged();
+    /* Signalled when the connection changes */
     void connectedChanged();
+    /* Signalled when a new output devices is chosen in the settings menu */
     void curOutChanged();
 public slots:
+    /* Start a UDP broadcast for the backend to find*/
     void beginUDPBroadcast();
+    /* Send some data to the backend over TCP */
     void tcpSend(QByteArray);
+    /* Start a UDP broadcast, retrieve the backend's IP, and connect to it over TCP */
     void udpRecvBackendIpAndConnect();
+    /* Get the list of IO devices from the backend */
     void getPorts();
+    /* Send the selected IO devices to the backend */
     void sendPorts();
     void updatePorts();
     void handleUDPStateChanged( QAbstractSocket::SocketState t_state );
